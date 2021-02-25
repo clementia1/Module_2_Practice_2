5ac62ca931d0c610904ca6506eaa95ddc370c3ba
clementia1 <grininma0@gmail.com>
2021-02-25 23:29:43 +0200

    initial structure

---
+++ Module_2_Practice_2/Models/Animals/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice_2.Models.Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age => DateTime.UtcNow.Year - BirthDate.Year;
+    }
+}
---
+++ Module_2_Practice_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_2_Practice_2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
