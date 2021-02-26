7356ca073d81b96baeb86b5bf6800149f463d370
Михаил Гринин <Grinin@kh.dcz.gov.ua>
2021-02-26 15:43:15 +0200

    added some interfaces

--- Module_2_Practice_2/Models/ParkArea.cs
+++ Module_2_Practice_2/Models/ParkArea.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Module_2_Practice_2.Services
+namespace Module_2_Practice_2.Models
 {
-    class AreaService
+    class ParkArea
     {
     }
 }
---
+++ Module_2_Practice_2/Services/Interfaces/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice_2.Services.Interfaces
+{
+    public interface IVisitor
+    {
+        void Observe();
+
+        void TakePhoto();
+    }
+}
